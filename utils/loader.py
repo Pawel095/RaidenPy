eae7a035b00ec5b8c4a22628ef40756869d531d1
Paweł 'Pawel095' Pankiewicz <pawelp6020@gmail.com>
2020-01-24 23:25:38 +0100

    updated loader

--- utils/loader.py
+++ utils/loader.py
@@ -6,14 +6,18 @@ loadingFinished = False
 
 
 class worker(threading.Thread):
-    def __init__(self, loadFunc, path, key):
+    def __init__(self, loadFunc, path, key,args=None):
         super().__init__()
         self.loadFunc = loadFunc
         self.path = path
         self.key = key
+        self.args=args
 
     def run(self):
-        assets[self.key] = self.loadFunc(self.path)
+        if self.args is not None:
+            assets[self.key] = self.loadFunc(self.path,*self.args)
+        else:
+            assets[self.key] = self.loadFunc(self.path)
 
 
 class Loader():
@@ -26,12 +30,18 @@ class Loader():
              worker(arcade.load_texture, "assets/enemy3.png", "enemy3"),
              worker(arcade.load_texture, "assets/laser1.png", "laser1"),
              worker(arcade.load_texture, "assets/laser2.png", "laser2"),
-             worker(arcade.load_texture, "assets/player.png", "player")
+             worker(arcade.load_texture, "assets/player.png", "player"),
+             worker(arcade.load_sound,"assets/laserShot1.wav","shot1"),
+             worker(arcade.load_sound,"assets/laserShot2.wav","shot2"),
+             worker(arcade.load_sound,"assets/laserShot3.wav","shot3"),
+             worker(arcade.load_sound,"assets/laserShot4.wav","shot4"),
+             worker(arcade.load_sound,"assets/laserShot5.wav","shot5"),
+             worker(arcade.load_sound,"assets/laserShot6.wav","shot6"),
+             worker(arcade.load_spritesheet,"assets/explosion.png","explosion",[64,64,4,16])
              ])
         [t.start() for t in threads]
         [t.join() for t in threads]
         loadingFinished = True
-        print(assets)
 
         if loadingFinished:
             pass
